bb711710106462dae286dcdf33af42caa5cfef8c
danbro <710170342@qq.com>
2020-03-18 11:23:25 +0800

    first commit

---
+++ Creational-Pattern/src/singleton/Singleton1.java
@@ -0,0 +1,23 @@
+package singleton;
+
+/**
+ * @Classname Singleton1
+ * @Description TODO 单例模式之饿汉模式
+ * @Date 2020/3/18 11:18
+ * @Author Danrbo
+ */
+
+/**
+ * 1、静态实例在类被加载的时候创建，保证线程安全
+ * 2、构造器被私有化，用户无法通过构造器创建对象
+ */
+public class Singleton1 {
+    private static Singleton1 instance = new Singleton1();
+
+    private Singleton1() {
+    }
+
+    public static Singleton1 getInstance() {
+        return instance;
+    }
+}
